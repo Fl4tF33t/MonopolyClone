6c268402c00899e544e0779bba33d48cbac3ae27
Tony Eriksson <ab5812@student.jamk.fi>
2023-01-28 15:07:56 +0200

    Networking
    Made the player a network object and added it as a prefab to the networkManager as a player prefab. Then I changed the movement to be updated on serverside. in the Totem.cs

--- Assets/Scripts/Totem.cs
+++ Assets/Scripts/Totem.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class Totem : MonoBehaviour
+public class Totem : NetworkBehaviour
 {
     public Route currentRoute;
     public int routePosition;
@@ -11,6 +12,11 @@ public class Totem : MonoBehaviour
     public float movementSpeed;
     bool isMoving;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentRoute = GameObject.Find("Route").GetComponent<Route>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -38,8 +44,10 @@ public class Totem : MonoBehaviour
             routePosition %= currentRoute.childNodeList.Count;
 
             Vector3 nextPos = currentRoute.childNodeList[routePosition].position;
-            while (MoveToNextNode(nextPos))
+            while (transform.position != nextPos)
             {
+                moveServerRpc(nextPos);
+
                 yield return null;
             }
             yield return new WaitForSeconds(0.2f);
@@ -51,14 +59,17 @@ public class Totem : MonoBehaviour
         
     }
 
+    //ServerRpc needs to be a void so it can not be done inside a IEnumerator
+    [ServerRpc]
+    void moveServerRpc(Vector3 goal)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, goal, movementSpeed * Time.deltaTime);
+    }
+
+    /*
     bool MoveToNextNode(Vector3 goal)
     {
         return goal != (transform.position = Vector3.MoveTowards(transform.position, goal, movementSpeed * Time.deltaTime));
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    */
 }
